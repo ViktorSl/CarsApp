cbd7ca5c13e5d97700801dae6ab279e432c15b8f
Viktor <viktorbox.84@gmail.com>
2020-03-16 20:12:01 +0200

    Updates

---
+++ precache-manifest.c2c9076dde87ce38db8f8237beac3210.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d6699e563a9c7f5d8bee0044b16a0cd",
+    "url": "/CarsApp/index.html"
+  },
+  {
+    "revision": "4e1019020272980c58f6",
+    "url": "/CarsApp/static/css/main.5b0f48c1.chunk.css"
+  },
+  {
+    "revision": "9b6993a0f039f03a2174",
+    "url": "/CarsApp/static/js/2.9bfb5acd.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/CarsApp/static/js/2.9bfb5acd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e1019020272980c58f6",
+    "url": "/CarsApp/static/js/main.e17c2bdf.chunk.js"
+  },
+  {
+    "revision": "f3ec48ac414ffe13ba21",
+    "url": "/CarsApp/static/js/runtime-main.ad6ab0bc.js"
+  }
+]);
